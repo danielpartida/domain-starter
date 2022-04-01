6966f40693bfdd2d395e728a9b20bc5f33ad3fe7
danielpartida <daniel.partida@d-fine.de>
2022-04-01 11:11:56 +0200

    Implement method renderMints()

--- src/App.js
+++ src/App.js
@@ -292,6 +292,47 @@ const App = () => {
 		}
 	}
 
+	const renderMints = () => {
+		if (currentAccount && mints.length > 0) {
+			return (
+				<div className="mint-container">
+					<p className='subtitle'>Recently minted domains!</p>
+					<div className='mint-list'>
+						{ mints.map((mint, index) => {
+           				return (
+              				<div className="mint-item" key={index}>
+                				<div className='mint-row'>
+                  					<a className="link" 
+									  href={`https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${mint.id}`} 
+									  target="_blank" rel="noopener noreferrer">
+                    				<p className="underlined">{' '}{mint.name}{tld}{' '}</p>
+                  					</a>
+                  					{/* If mint.owner is currentAccount, add an "edit" button*/}
+                  					{ mint.owner.toLowerCase() === currentAccount.toLowerCase() ?
+                    				<button className="edit-button" onClick={() => editRecord(mint.name)}>
+										<img className="edit-icon" 
+										src="https://img.icons8.com/metro/26/000000/pencil.png" 
+										alt="Edit button" />
+                    				</button>
+                    				:
+                   	 				null
+                  					}
+								</div>
+							<p> {mint.record} </p>
+						</div>)
+					})}
+					</div>
+				</div>
+			);
+		}
+	};
+
+	const editRecord = (name) => {
+		console.log("Editing record for", name);
+		setEditing(true);
+		setDomain(name);
+	  }
+
 	// this runs the function when the page loads
 	useEffect(() => {
 		checkIfWalletIsConnected();
